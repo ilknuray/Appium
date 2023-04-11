ab47485aed48bd3c4753ff4970439a374656a424
ilknur yilmaz <ilknuray1318@gmail.com>
2023-04-11 21:56:06 +0200

    Ap1

---
+++ src/test/java/Appium/Appium01.java
@@ -0,0 +1,29 @@
+package Appium;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Test;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Appium01 {
+    @Test
+    public void test() throws MalformedURLException {
+        DesiredCapabilities capabilities=new DesiredCapabilities();
+        //capabilities.setCapability("platformName","Android");Seleniumdan
+        //capabilities.setCapability(CapabilityType.PLATFORM_NAME,"Android");Appiumdan
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"RealDevice");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+
+        AndroidDriver<MobileElement>driver=new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+
+
+
+    }
+}
