0341eea3522b8cf02ebed5801dbd903cec9dccf8
ilknur yilmaz <ilknuray1318@gmail.com>
2023-04-11 23:17:27 +0200

    Ap1

--- src/test/java/Appium/Appium01.java
+++ src/test/java/Appium/Appium01.java
@@ -13,16 +13,17 @@ import java.net.URL;
 public class Appium01 {
     @Test
     public void test() throws MalformedURLException {
-        DesiredCapabilities capabilities=new DesiredCapabilities();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
         //capabilities.setCapability("platformName","Android");Seleniumdan
         //capabilities.setCapability(CapabilityType.PLATFORM_NAME,"Android");Appiumdan
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"RealDevice");
-        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-
-        AndroidDriver<MobileElement>driver=new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
-
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.APP, "C:/Users/fylmz/IdeaProjects/MobileTest/src/Apps/apiDemos.apk");
+        capabilities.setCapability("appPackage","com.touchboarder.android.api.demos");
+        capabilities.setCapability("appActivity","com.touchboarder.androidapidemos.MainActivity");
+        AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 
 
     }
