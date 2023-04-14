16a11de06f6598d9caada861f7caa2678a0e4a6f
ilknur yilmaz <ilknuray1318@gmail.com>
2023-04-14 19:14:20 +0200

    Ap1

--- src/test/java/Appium/Appium2.java
+++ src/test/java/Appium/Appium2.java
@@ -1,12 +1,17 @@
 package Appium;
 
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Appium2 {
     @Test
-    public void test(){
+    public void test() throws MalformedURLException, InterruptedException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         //capabilities.setCapability("platformName","Android");Seleniumdan
         //capabilities.setCapability(CapabilityType.PLATFORM_NAME,"Android");Appiumdan
@@ -14,7 +19,11 @@ public class Appium2 {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-        capabilities.setCapability(MobileCapabilityType.APP, "C:/Users/fylmz/IdeaProjects/MobileTest/src/Apps/com.google.android.calculator_78000303_apps.evozi.com.apk");
+        capabilities.setCapability("noReset",true);
+        capabilities.setCapability(MobileCapabilityType.APP, "C:/Users/fylmz/IdeaProjects/MobileTest/src/Apps/Calculator.apk");
+        Thread.sleep(5000);
+        AndroidDriver<MobileElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        Thread.sleep(5000);
 
     }
 }
