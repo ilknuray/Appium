bf49240dfc0a201284f334072eaca3413dcffa12
ilknur yilmaz <ilknuray1318@gmail.com>
2023-04-14 21:48:18 +0200

    Ap1

--- src/test/java/Appium/Appium3.java
+++ src/test/java/Appium/Appium3.java
@@ -11,16 +11,21 @@ import java.net.URL;
 
 public class Appium3 {
     @Test
-    public void test01() throws MalformedURLException {
+    public void test01() throws MalformedURLException, InterruptedException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-
+        capabilities.setCapability(MobileCapabilityType.APP, "C:/Users/fylmz/IdeaProjects/MobileTest/src/Apps/Calculator.apk");
         capabilities.setCapability("appPackage","com.android.chrome");
         capabilities.setCapability("appActivity","org.chromium.chrome.browser.ChromeTabbedActivit");
+        capabilities.setCapability("noReset",true);
         AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        Thread.sleep(5000);
+        MobileElement acceptContinue=driver.findElementById("com.android.chrome:id/terms_accept");
+        acceptContinue.click();
+        driver.closeApp();
     }
 }
