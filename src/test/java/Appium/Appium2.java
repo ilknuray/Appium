781e3d5a275e4d83d92855ce984a2eb52cb02ece
ilknur yilmaz <ilknuray1318@gmail.com>
2023-04-14 21:15:24 +0200

    Ap1

--- src/test/java/Appium/Appium2.java
+++ src/test/java/Appium/Appium2.java
@@ -12,13 +12,16 @@ import java.net.URL;
 public class Appium2 {
     @Test
     public void test() throws MalformedURLException, InterruptedException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
         //capabilities.setCapability("platformName","Android");Seleniumdan
         //capabilities.setCapability(CapabilityType.PLATFORM_NAME,"Android");Appiumdan
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        //capabilities.setCapability("appPackage","com.touchboarder.android.calculator");
+       // capabilities.setCapability("appActivity","com.touchboarder.calculator.MainActivity");
         capabilities.setCapability("noReset",true);
         capabilities.setCapability(MobileCapabilityType.APP, "C:/Users/fylmz/IdeaProjects/MobileTest/src/Apps/Calculator.apk");
         Thread.sleep(5000);
---
+++ src/test/java/Appium/Appium3.java
@@ -0,0 +1,26 @@
+package Appium;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Appium3 {
+    @Test
+    public void test01() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+
+        capabilities.setCapability("appPackage","com.android.chrome");
+        capabilities.setCapability("appActivity","org.chromium.chrome.browser.ChromeTabbedActivit");
+        AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+}
