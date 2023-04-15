4bef54824dc0239782ebd9e0adaa32ed4c74a782
ilknur yilmaz <ilknuray1318@gmail.com>
2023-04-15 15:43:55 +0200

    Ap1

---
+++ src/test/java/Appium/Appium4.java
@@ -0,0 +1,31 @@
+package Appium;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Appium4 {
+    @Test
+    public void test() throws InterruptedException, MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        capabilities.setCapability("noReset", true);
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,"chrome");
+        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT,60000);
+        capabilities.setCapability("chromeDriverExecutable","C:/Users/fylmz/IdeaProjects/MobileTest/src/driver/chromedriver.exe");
+        Thread.sleep(5000);
+        AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        Thread.sleep(5000);
+
+        driver.get("https://www.amazon.com");
+    }
+}
