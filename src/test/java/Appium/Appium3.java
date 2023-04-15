e497a136b489b4de8064bc366295722234040702
ilknur yilmaz <ilknuray1318@gmail.com>
2023-04-15 14:57:37 +0200

    Ap1

--- src/test/java/Appium/Appium3.java
+++ src/test/java/Appium/Appium3.java
@@ -4,6 +4,7 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.Test;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
@@ -11,21 +12,24 @@ import java.net.URL;
 
 public class Appium3 {
     @Test
-    public void test01() throws MalformedURLException {
+    public void test01() throws MalformedURLException, InterruptedException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-        capabilities.setCapability(MobileCapabilityType.APP, "C:/Users/fylmz/IdeaProjects/MobileTest/src/Apps/General-Store.apk");
-        capabilities.setCapability("appPackage","com.android.chrome");
-        capabilities.setCapability("appActivity","org.chromium.chrome.browser.ChromeTabbedActivit");
-        capabilities.setCapability("noReset",true);
+        capabilities.setCapability("noReset", true);
+        capabilities.setCapability(MobileCapabilityType.APP, "C:/Users/fylmz/IdeaProjects/MobileTest/src/Apps/Calculator.apk");
+        capabilities.setCapability("appPackage", "com.android.chrome");
+        capabilities.setCapability("appActivity", "com.google.android.apps.chrome.Main");
+        Thread.sleep(5000);
         AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-        //Thread.sleep(5000);
-        MobileElement acceptContinue=driver.findElementById("com.android.chrome:id/terms_accept");
+        driver.get("https://www.amazon.com");
+        Thread.sleep(5000);
+        MobileElement acceptContinue = driver.findElementById("com.android.chrome:id/url_bar");
         acceptContinue.click();
+        acceptContinue.sendKeys("Telephone"+Keys.ENTER);
         driver.closeApp();
     }
 }
