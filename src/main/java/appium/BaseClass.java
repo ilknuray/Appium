67c86046c8d555b99662b939f6dab9014e9cf542
ilknur yilmaz <ilknuray1318@gmail.com>
2023-04-17 19:09:17 +0200

    Ap1

---
+++ src/main/java/appium/BaseClass.java
@@ -0,0 +1,29 @@
+package appium;
+
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BaseClass {
+    @Test
+    public AndroidDriver getAndroidDriver() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.APP, "C:/Users/fylmz/IdeaProjects/MobileTest/src/Apps/apiDemos.apk");
+       // capabilities.setCapability("appPackage","com.touchboarder.android.api.demos");
+       // capabilities.setCapability("appActivity","com.touchboarder.androidapidemos.MainActivity");
+        capabilities.setCapability("noReset",true);
+        AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        return driver;
+    }
+}
---
+++ src/test/java/Appium/Appium7.java
@@ -0,0 +1,15 @@
+package Appium;
+
+import appium.BaseClass;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+
+public class Appium7 extends BaseClass {
+    @Test
+    public void test() throws MalformedURLException {
+     AndroidDriver driver=getAndroidDriver();
+    }
+
+}
